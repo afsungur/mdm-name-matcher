5abb4c1f9642faf9d00cb435cf23d4eba31c7e8f
Ahmet Fuat Sungur <fuat.sungur@Fuats-MacBook-Pro.local>
2020-10-11 08:15:16 +0400

    Small changes

--- name_matching.py
+++ name_matching.py
@@ -38,18 +38,18 @@ for i in range(M):
 # If you have small distance matrix you can print and see the results
 #print(distance_matrix)
 
-# User friendly similarity threshold
+# User friendly similarity threshold - normally scikit looks for the maximum distance threshold to get the 2 different points merged
 similarity_threshold=75
 
-# execute single-linkage clustering algorithm
+# execute single-linkage clustering algorithm based on distance matrix which has been computed by checking the similarity between of every records
 clustering = AgglomerativeClustering(n_clusters=None, affinity="precomputed", compute_full_tree=True, linkage="single", distance_threshold=(100-similarity_threshold)/100).fit(distance_matrix)
 
 # Show how many clusters are generated
 # If the value is equal to number of customers then no merge operation happened
-print(clustering.n_clusters_)
+print(f"Number of clusters: {clustering.n_clusters_}")
 
 # Show which data point belongs to which cluster -- it is 1 dimensional array
-print(clustering.labels_)
+#print(clustering.labels_)
 
 # Print the cluster 0 elements -- which records are merged for the cluster 0
 for i in range(len(clustering.labels_)):
@@ -57,21 +57,28 @@ for i in range(len(clustering.labels_)):
             print(f"Name: {fullNameArray[i][0]}")
             print(f"Id: {fullNameArray[i][1]}")
 
+print("Merging started ...")
 # Merging the records into master_customers collection together with similar customer records and their transactions
+
+# Since it is one time merge we delete every records firstly 
 master_collection.delete_many({})
 for i in range(len(clustering.labels_)):
-    index="MSTR"+str(clustering.labels_[i])
-    customer_doc=collection.find_one({"_id":fullNameArray[i][1]})
+    master_record_id="MSTR"+str(clustering.labels_[i])
+
+     
+    customer_doc=collection.find_one({"_id":fullNameArray[i][1]}) 
+    
+    # get all the transactions of source customer
     tx_cursor=tx_collection.find({"customerId": fullNameArray[i][1]})
     transactions=[]
     for tx in tx_cursor:
         transactions.append(tx)
 
     master_collection.update_one(
-            {"_id":index},
+            {"_id":master_record_id},
             {
                 '$inc': {'numberOfSourceCustomers':1}, 
                 '$push': {'customers': customer_doc, 'transactions': {'$each': transactions} }
             },upsert=True)
 
-
+print("Merging completed ...")
